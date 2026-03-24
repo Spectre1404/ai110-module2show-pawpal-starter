f7ec77a6db74fc79a0a7f51780f98ae3048e33c5
Shivansh Amba <shivanshamba1404@gmail.com>
2026-03-23 20:41:05 -0500

    Logic and UI connected

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from datetime import datetime, date
+from pawpal_system import Owner, Pet, Task, Scheduler
 
 st.set_page_config(page_title="PawPal+", page_icon="🐾", layout="centered")
 
